f0767c635121bda5c15769cd64a352cece31a5fa
Vyvy-vi <vyom.j@protonmail.com>
2021-03-13 12:53:38 +0530

    feat: added xor logic

--- hamming-codes/hamming.py
+++ hamming-codes/hamming.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from functools import reduce
 
 
@@ -11,9 +12,21 @@ def print_bit_block(arr):
     char = int(len(arr) ** 0.5)
     print(f'\n'.join([''.join(arr[i: i+char]) for i in range(0, len(arr), char)]))
 
+def parity_bits(block_size):
+    bits = int(math.log(block_size, 2))
+    print(f'No. of parity bits: {bits}\nNo of data bits: {block_size - bits}')
 
-if len(bits) <= 10 ** 6:
-    bits = generate_bit_block(eval(input('Enter no. of bits: ')))
+def hamming_check(bits):
+    ls = reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(bits) if bit])
+    return ls
+
+
+bits = eval(input('Enter no. of bits: '))
+if bits <= 2 ** 14:
+    parity_bits(bits)
+    bits = generate_bit_block(bits)
     print_bit_block(bits)
+    print('This block of bits may or may not have any errors')
+    print(hamming_check(bits))
 else:
     print('num too large')
